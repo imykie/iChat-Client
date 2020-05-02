04ee6ed90d22b4fa41dbc2e543259e5aaaa16236
Michael Olatunji <tundexmike@gmail.com>
2020-05-02 23:42:22 +0100

    graphql package added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
-import 'materialize-css/dist/css/materialize.min.css'
-import M from 'materialize-css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'materialize-css/dist/css/materialize.min.css';
+// import M from 'materialize-css';
 import * as serviceWorker from './serviceWorker';
-import Firebase, { FirebaseContext }from './components/Firebase';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
   <React.StrictMode>
